5ddffd8486c869e827ad33a89fa747a0b2520a0f
Umidjon <mrumidjon2020@gmail.com>
2022-04-10 23:10:05 +0500

    Find the each row length. 01

--- file06.py
+++ file06.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data: str):
     """
     The data is from the file. Find the each row length and return as list type.
     Args:
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    res = []
+    for i in data.split("\n"):
+        res.append(len(i))
+    return res
+
+
 # Read data from file
+
+f = open('txt_file/data06.txt', 'r')
+print(main(f.read()))
+f.close()
