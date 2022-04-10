bcbe4c3edb5474e70d1eb4351c4e1ef84b7107f5
Umidjon <mrumidjon2020@gmail.com>
2022-04-10 23:06:02 +0500

    Find the number of digital and str(non-digital) data. 01

--- file05.py
+++ file05.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data: str):
     """
     The data is from the file. Find the number of digital and str(non-digital) data and return as list type.
     Args:
@@ -6,5 +6,17 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    res = [0, 0]
+    for i in data:
+        if i.isdigit():
+            res[0] += 1
+        else:
+            res[1] += 1
+    return res
+
+
 # Read data from file
+
+f = open('txt_file/data05.txt', 'r')
+print(main(f.read()))
+f.close()
