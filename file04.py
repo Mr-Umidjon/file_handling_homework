b648649fae96a094dc6e5003f526d8cade2f3eae
Umidjon <mrumidjon2020@gmail.com>
2022-04-10 23:00:27 +0500

    Return the str(non-digital) characters as a list type. 01

--- file04.py
+++ file04.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data: str):
     """
     The data is from the file. Return the str(non-digital) characters as a list type.
     Args:
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    res = []
+    for i in data:
+        if not i.isdigit():
+            res.append(i)
+    return res
+
+
 # Read data from file
+f = open("txt_file\data04.txt", 'r')
+print(main(f.read()))
