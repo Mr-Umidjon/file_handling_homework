315d2cec322c3fcf6ffd32e67cb2568ecf5b2a9a
Umidjon <mrumidjon2020@gmail.com>
2022-04-10 22:48:40 +0500

    Read data from file 01

--- file01.py
+++ file01.py
@@ -6,5 +6,11 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    f = open(data, 'r')
+    res = []
+    for i in f.read():
+        if i.isdigit():
+            res.append(int(i))
+    return res
 
 # Read data from file
