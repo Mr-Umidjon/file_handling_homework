dd70a2b7e4a5930a9aa8c2888bfbe45a15ec71c9
Umidjon <mrumidjon2020@gmail.com>
2022-04-10 23:22:07 +0500

    Find the each row length and return the largest row. 01

--- file10.py
+++ file10.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data: str):
     """
     The data is from the file. Find the each row length and return the largest row.
     Args:
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    res = 0
+    for i in data.split("\n"):
+        i = len(i)
+        if i > res:
+            res = i
+    return res
+
 
 # Read data from file
+f = open('txt_file/data10.txt', 'r')
+print(main(f.read()))
+f.close()
